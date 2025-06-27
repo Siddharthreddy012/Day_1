71d68527bd2203fd06426d7e38e6f5c4836726b6
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-27 12:55:52 +0530

    conversion_num

---
+++ Binary.py
@@ -0,0 +1,3 @@
+#d=76488683487217 to bin 
+d=76488683487217
+print("binary format of D is :",bin(d))
---
+++ Octal.py
@@ -0,0 +1,3 @@
+#e=906755345674392787394 to oct 
+e=906755345674392787394
+print("octal format of E is :",oct(e))
